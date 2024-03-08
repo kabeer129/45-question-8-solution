da01d2b7e282c128e38fa73126a8982da4be9d85
abdul kabeer khan <162288997+kabeer129@users.noreply.github.com>
2024-03-08 06:02:51 -0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,37 @@
+//ALL 45 QUESTION SOLUTION
+//QUESTION 1 COMPLETED
+
+//QUESTION 2 
+//let personname: string ="eric";
+//console.log('hello {eric}, would you like to learn some python today?');
+
+//QUESTION 3
+//let personname: string = "Abdul kabeer khan";
+//console.log("lowercase:", personname.toLowerCase() );
+
+//QUESTION 4
+//let quote: string ="A person who never made a mistake never tried anything new.";
+//let author: string="ALBERT EINSTEIN";
+//console.log(`${author} once said,"${quote}"`);
+
+//QUESTION 5
+//let famous_person: string = "ALBERT EINSTEIN";
+//let message: string = `${famous_person} once said,"${quote}"`;
+//console.log("A person who never made a mistake never tried anything new")
+
+//QUESTION 6
+//let personname: string = "\t\n john doe \n\t";
+//console.log("original:",personname);
+
+//QUESTION 7
+//console.log(9+7);
+//console.log(11-1);
+//console.log(10*2);
+//console.log(2/20);
+
+//QUESTION 8
+//console.log(5+3);
+
+
+
+
